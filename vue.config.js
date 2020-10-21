b0544c4908d408e1fa3b864a1ead918e852bf65d
alifanantomi <alifanantomi2@gmail.com>
2020-10-21 18:39:28 +0700

    remove gh-pages

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,6 @@ module.exports = {
   runtimeCompiler: true,
   "transpileDependencies": [
     "vuetify"
-  ],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/task-app/'
-    : '/'
+  ]
 }
 
